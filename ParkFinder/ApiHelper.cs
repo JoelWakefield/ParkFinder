ef0b237dc5c3763f8fe9916902ce54fdce4a78dc
Joel Wakefield <joel.wakefield@improving.com>
2021-06-23 08:20:20 -0500

    first commit

---
+++ ParkFinder/ApiHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParkFinder.Models;
+
+namespace ParkFinder
+{
+    public class ApiHelper
+    {
+        public static List<Park> GetData(string uri)
+        {
+            List<Park> items = new List<Park>();
+
+
+
+            return items;
+        }
+    }
+}
---
+++ ParkFinder/Models/Park.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkFinder.Models
+{
+    public class Park
+    {
+        public string ParkID { get; set; }
+        public string ParkName { get; set; }
+        public string SanctuaryName { get; set; }
+        public string Borough { get; set; }
+        public int Acres { get; set; }
+        public string Directions { get; set; }
+        public string Description { get; set; }
+        public string HabitatType { get; set; }
+    }
+}
