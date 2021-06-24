66eefec37a78228eee00c85d08f957c664479bd5
Joel Wakefield <joel.wakefield@improving.com>
2021-06-24 10:03:50 -0500

    refactored filtering and logging

--- ParkFinder/ApiHelper.cs
+++ ParkFinder/ApiHelper.cs
@@ -7,25 +7,31 @@ using System.Threading.Tasks;
 using ParkFinder.Models;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 
 namespace ParkFinder
 {
     public class ApiHelper
     {
+        private readonly ILogger<ApiHelper> _logger;
         private readonly IMemoryCache _memoryCache;
 
-        public ApiHelper(IMemoryCache memoryCache)
+        private readonly string ApiUrl = "https://seriouslyfundata.azurewebsites.net/api/parks";
+        private readonly string cacheKey = "parks";
+
+        public ApiHelper(ILogger<ApiHelper> logger, IMemoryCache memoryCache)
         {
+            _logger = logger;
             _memoryCache = memoryCache;
         }
 
-        public async Task<List<Park>> GetData(string uri)
+        private async Task<List<Park>> GetData()
         {
             List<Park> items = new List<Park>();
 
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri(uri);
+                client.BaseAddress = new Uri(ApiUrl);
 
                 client.DefaultRequestHeaders.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -35,35 +41,59 @@ namespace ParkFinder
                 if (response.IsSuccessStatusCode)
                 {
                     var result = response.Content.ReadAsStringAsync().Result;
-
                     items = JsonConvert.DeserializeObject<List<Park>>(result);
+
+                    _logger.LogInformation("successful pull from api");
+                }
+                else
+                {
+                    _logger.LogInformation("failure to pull from api");
                 }
             }
 
             return items;
         }
 
-        public async Task<List<Park>> GetCache(string url)
+        public async Task<List<Park>> Get(string search)
         {
-            string cachKey = "Parks";
             List<Park> parks;
 
-            if (!_memoryCache.TryGetValue(cachKey, out parks))
+            if (!_memoryCache.TryGetValue(cacheKey, out parks))
             {
                 //  fetch value
-                parks = await GetData(url);
+                parks = await GetData();
 
                 //  store in the cache
                 _memoryCache.Set(
-                    cachKey,
+                    cacheKey,
                     parks,
                     new MemoryCacheEntryOptions()
                         .SetSlidingExpiration(TimeSpan.FromMinutes(5))
                         .SetAbsoluteExpiration(TimeSpan.FromHours(1))
                 );
+
+                _logger.LogInformation($"cache {cacheKey} being filled");
+            }
+            else
+            {
+                _logger.LogInformation($"cache {cacheKey} already full");
             }
 
-            return parks;
+            //  filter out results
+            if (search != null)
+            {
+                _logger.LogInformation("returning filtered data");
+
+                return parks.Where(p =>
+                {
+                    return p.ParkName.Contains(search) || p.Description.Contains(search);
+                }).ToList();
+            }
+            else
+            {
+                _logger.LogInformation("returning all data");
+                return parks;
+            }
         }
     }
 }
--- ParkFinder/Controllers/ParkDataController.cs
+++ ParkFinder/Controllers/ParkDataController.cs
@@ -11,34 +11,16 @@ namespace ParkFinder.Controllers
 {
     public class ParkDataController : Controller
     {
-        private readonly ILogger<ParkDataController> _logger;
         private readonly ApiHelper _apiHelper;
 
-        private readonly string ApiUrl = "https://seriouslyfundata.azurewebsites.net/api/parks";
-
-        public ParkDataController(ILogger<ParkDataController> logger, ApiHelper apiHelper)
+        public ParkDataController(ApiHelper apiHelper)
         {
-            _logger = logger;
             _apiHelper = apiHelper;
         }
         
         public async Task<IActionResult> Index(string search)
         {
-            //  Get data
-            List<Park> parks = await _apiHelper.GetCache(ApiUrl);
-
-            //  filter out results
-            if (search != null)
-            {
-                ViewBag.Parks = parks.Where(p =>
-                {
-                    return p.ParkName.Contains(search) || p.Description.Contains(search);
-                }).ToList();
-            }
-            else
-            {
-                ViewBag.Parks = parks;
-            }
+            ViewBag.Parks = await _apiHelper.Get(search);
 
             return View();
         }
