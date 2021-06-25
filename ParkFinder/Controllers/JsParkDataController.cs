01a03c52a67b7e8e6687eda82a672290229955bd
Joel Wakefield <joel.wakefield@improving.com>
2021-06-25 00:05:49 -0500

    well, I dont get it, but its just not working

---
+++ ParkFinder/Controllers/JsParkDataController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ParkFinder.Models;
+
+namespace ParkFinder.Controllers
+{
+    public class JsParkDataController : Controller
+    {
+        private readonly ApiHelper _apiHelper;
+
+        public JsParkDataController(ApiHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Parks = await _apiHelper.Get();
+
+            return View();
+        }
+
+        public async Task<IActionResult> GetParksAsync(string query)
+        {
+            var data = await _apiHelper.Get(query);
+
+            return Json(data);
+        } 
+    }
+}
